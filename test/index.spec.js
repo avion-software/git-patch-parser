73302cbd83c7f9e425680c1546658c7c19da7ae5
Michael Braun <michael@web4brauns.de>
2020-03-11 22:25:51 +0100

    Add test for removed-file

--- test/index.spec.js
+++ test/index.spec.js
@@ -6,6 +6,7 @@ import parsePatch from '../src';
 import simpleChangeLine from '../test-data/simple-change-line';
 import simpleAddLine from '../test-data/simple-add-line';
 import simpleRemoveLine from '../test-data/simple-remove-line';
+import simpleRemoveFile from '../test-data/simple-remove-file';
 
 describe('lines', () => {
     it('change-line', async () => {
@@ -24,5 +25,11 @@ describe('lines', () => {
         const buffer = await fs.readFile(path.resolve(__dirname, '..', 'test-data', 'simple-remove-line.patch'));
 
         assert.deepEqual(parsePatch(buffer.toString()), simpleRemoveLine);
+    });
+
+    it('remove-file', async () => {
+        const buffer = await fs.readFile(path.resolve(__dirname, '..', 'test-data', 'simple-remove-file.patch'));
+
+        assert.deepEqual(parsePatch(buffer.toString()), simpleRemoveFile);
     });
 });
