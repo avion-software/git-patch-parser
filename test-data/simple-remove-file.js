4a4cefa16565b19eabd6c69b0e1965a53a4e4ddb
Michael Braun <michael@web4brauns.de>
2020-03-11 22:24:51 +0100

    Add simple-remove-file test-data

---
+++ test-data/simple-remove-file.js
@@ -0,0 +1,48 @@
+export default {
+    files: [{
+        source: 'a/test.txt',
+        target: null,
+        type: 'removed',
+        hunks: [{
+            source: {
+                from: 1,
+                to: 5,
+            },
+            target: {
+                from: 0,
+                to: 0,
+            },
+            lines: [{
+                type: 'removed',
+                line: {
+                    source: 1,
+                    target: 0,
+                }
+            }, {
+                type: 'removed',
+                line: {
+                    source: 2,
+                    target: 0,
+                }
+            }, {
+                type: 'removed',
+                line: {
+                    source: 3,
+                    target: 0,
+                }
+            }, {
+                type: 'removed',
+                line: {
+                    source: 4,
+                    target: 0,
+                }
+            }, {
+                type: 'normal',
+                line: {
+                    source: 5,
+                    target: 0,
+                }
+            }]
+        }]
+    }],
+};
