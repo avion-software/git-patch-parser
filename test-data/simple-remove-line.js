37218f0fedba072aab3635529836b98bc147c952
Michael Braun <michael@web4brauns.de>
2020-03-11 23:05:09 +0100

    Rename source/target to before/after in test-data
    It is clearer to use the before/after properties (before/after the patch) than source/target.

--- test-data/simple-remove-line.js
+++ test-data/simple-remove-line.js
@@ -1,7 +1,7 @@
 export default {
     files: [{
-        source: 'a/test.txt',
-        target: 'b/test.txt',
+        before: 'a/test.txt',
+        after: 'b/test.txt',
         meta: {
             index: {
                 before: '4ce0f7a',
@@ -13,43 +13,43 @@ export default {
             },
         },
         hunks: [{
-            source: {
+            before: {
                 from: 1,
                 to: 5,
             },
-            target: {
+            after: {
                 from: 1,
                 to: 4,
             },
             lines: [{
                 type: 'normal',
                 line: {
-                    source: 1,
-                    target: 1,
+                    before: 1,
+                    after: 1,
                 }
             }, {
                 type: 'normal',
                 line: {
-                    source: 2,
-                    target: 2,
+                    before: 2,
+                    after: 2,
                 }
             }, {
                 type: 'normal',
                 line: {
-                    source: 3,
-                    target: 3,
+                    before: 3,
+                    after: 3,
                 }
             }, {
                 type: 'removed',
                 line: {
-                    source: 4,
-                    target: 3,
+                    before: 4,
+                    after: 3,
                 }
             }, {
                 type: 'normal',
                 line: {
-                    source: 5,
-                    target: 4,
+                    before: 5,
+                    after: 4,
                 }
             }]
         }]
