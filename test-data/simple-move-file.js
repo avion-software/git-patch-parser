9dccc7867d2d447c86c08abc245e4b30595a40bc
Michael Braun <michael@web4brauns.de>
2020-03-12 20:58:50 +0100

    Add test file for file moving

---
+++ test-data/simple-move-file.js
@@ -0,0 +1,10 @@
+export default {
+    files: [{
+        before: 'test.txt',
+        after: 'test2.txt',
+        type: 'renamed',
+        meta: {
+            similarity: 100,
+        },
+    }],
+};
