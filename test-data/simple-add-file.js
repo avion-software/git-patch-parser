4b0e00ed8f17b39b7bf4585cd13d299fa0ccce3d
Michael Braun <michael@web4brauns.de>
2020-03-14 00:16:01 +0100

    Add add-file test-data

---
+++ test-data/simple-add-file.js
@@ -0,0 +1,57 @@
+export default {
+    files: [{
+        before: null,
+        after: 'b/test.txt',
+        type: 'added',
+        meta: {
+            index: {
+                before: '0000000',
+                after: '46a2c46',
+            },
+            mode: {
+                after: 100644,
+            },
+        },
+        hunks: [{
+            before: {
+                from: 0,
+                to: 0,
+            },
+            after: {
+                from: 1,
+                to: 5,
+            },
+            lines: [{
+                type: 'added',
+                line: {
+                    before: 0,
+                    after: 1,
+                }
+            }, {
+                type: 'added',
+                line: {
+                    before: 0,
+                    after: 2,
+                }
+            }, {
+                type: 'added',
+                line: {
+                    before: 0,
+                    after: 3,
+                }
+            }, {
+                type: 'added',
+                line: {
+                    before: 0,
+                    after: 4,
+                }
+            }, {
+                type: 'added',
+                line: {
+                    before: 0,
+                    after: 5,
+                }
+            }]
+        }]
+    }],
+};
